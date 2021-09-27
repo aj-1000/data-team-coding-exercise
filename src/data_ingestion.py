3d2311c2e42690aa235d8ae3e766a1e8f2722fca
AJAJA <adafejaja@gmail.com>
2021-09-27 18:20:20 +0100

    add: type-checking

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,17 +1,38 @@
 import pandas as pd
+import pandera as pa
+import numpy as np
 
 from fastlogging import LogInit
 
 logger = LogInit(pathName='logs/data_ingestion.log')
 
+def check_dataframe_schema(df: pd.DataFrame, schema: pa.DataFrameSchema) -> None:
+    """A function to assert that the dataframe has the types expected in the 
+    schema"""
+    validate_df = schema.validate(df)
+
 def ingest_components_file(file: str) -> None:
     "A function to load, type check and save the components data from a file"
+    schema = pa.DataFrameSchema({
+        "componentId": pa.Column(str),
+        "colour": pa.Column(str),
+        "costPrice": pa.Column(float) 
+    })
+
     df = pd.read_csv(file)
+    check_dataframe_schema(df, schema)
     df.to_pickle('./data/df_components.pkl')
 
 def ingest_orders_file(file: str):
     "A function to load, type check and save the orders data from a file"
+    schema = pa.DataFrameSchema({
+        "timestamp": pa.Column(np.datetime64),
+        "orderId": pa.Column(str),
+        "units": pa.Column(object) 
+    })
+    
     df = pd.read_json(file, lines=True)
+    check_dataframe_schema(df, schema)
     df.to_pickle('./data/df_orders.pkl')
 
 def main(components_file: str, orders_file: str) -> None:
